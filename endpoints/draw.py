33da3816d34a09c62a00d87c5eb2c60d0bc9accb
Gabriel Galindo <gabriel.rodriguez@networkingdev.com>
2022-05-18 18:41:22 -0500

    Test for Cordova Fetch

--- endpoints/draw.py
+++ endpoints/draw.py
@@ -7,14 +7,14 @@ from models.drawModel import drawM
 
 
 router = APIRouter()
-base_url = "/home/ilana/others/carp_api/api/src/imgs"
+base_url = "http://localhost:8090/others/carp_api/api/src/imgs"
 
 @router.get("/test_answer")
 def device_get():
     return JSONResponse({'msg': 'The API connection is up'})
 
 
-@router.get("/get_image")
+@router.post("/get_image")
 def return_img(data: drawM):
     img_url = f"{base_url}/{data.collection}_{data.part}{data.variation}.png"
     print(img_url)
